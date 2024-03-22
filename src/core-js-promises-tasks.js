1ad42fe3171e25ef513598b39cabae0b0e2aac54
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-22 21:34:46 +0100

    feat: complete getAllOrNothing task and refactor getFirstResolvedPromiseResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -59,7 +59,7 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises);
+  return Promise.any(promises);
 }
 
 /**
@@ -82,7 +82,7 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
 function getFirstPromiseResult(promises) {
-  return Promise.any(promises);
+  return Promise.race(promises);
 }
 
 /**
@@ -96,8 +96,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
